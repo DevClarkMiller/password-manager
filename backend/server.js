69f3c1acbe832fc4a16665a58714aa09525c854c
Clark Miller <63131937+MeatBallFreak69@users.noreply.github.com>
2024-08-06 12:51:43 -0400

    Added the really basic setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
+
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 
-
-
-
-
+app.listen((port), () =>{
+    console.log(`✅ server up on port: ${port}`);
+});
